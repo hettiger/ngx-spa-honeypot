1c3483278a2111c05b9de9cdb7676db58f9ecb68
Martin Hettiger <martin@hettiger.com>
2022-12-11 00:17:12 +0100

    Install @angular/pwa

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { TemplateDrivenFormComponent } from './template-driven-form/template-dri
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { SPA_HONEYPOT_CONFIG, SpaHoneypotConfig } from '../../../ngx-spa-honeypot/src/lib/spa-honeypot.config';
 import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -58,6 +59,12 @@ import { environment } from '../environments/environment';
     MatCardModule,
     MatCheckboxModule,
     MatSlideToggleModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [
     {
